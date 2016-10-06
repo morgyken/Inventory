cf6ecc66ed817033d5d60c0c40a5a68a1e9dd56d
Samuel Odhiambo <sodhiambo@collabmed.com>
2016-10-06 13:02:36 +0300

    Fix mistake for routes

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -9,21 +9,11 @@
  *
  * =============================================================================
  */
+$router->get('products', ['uses' => 'ApiController@get_products', 'as' => 'get_products']);
+$router->get('get/products', ['uses' => 'ApiController@products', 'as' => 'get.products']);
+$router->get('getstock', ['uses' => 'ApiController@getitemstock', 'as' => 'getstock']);
+$router->get('customers/autofetch', ['uses' => 'ApiController@fetchCustomer', 'as' => 'cust.get']);
+$router->get('batch/amount', ['uses' => 'ApiController@fetchBatchAmount', 'as' => 'batch.amount']);
+$router->get('credit/rate', ['uses' => 'ApiController@creditSelling', 'as' => 'credit.rate']);
+$router->get('approve/adjustment', ['uses' => 'ApiController@approveStockAdjustment', 'as' => 'adj_approve']);
 
-
-$ajax = [
-    'namespace' => 'Ignite\Inventory\Http\Controllers',
-    'as' => 'inventory.ajax.',
-    'prefix' => 'inventory/ajax',
-    'middleware' => ['ajax'],
-];
-//AJAX ONLY routes
-Route::group($ajax, function() {
-    Route::get('products', ['uses' => 'ApiController@get_products', 'as' => 'get_products']);
-    Route::get('get/products', ['uses' => 'ApiController@products', 'as' => 'get.products']);
-    Route::get('getstock', ['uses' => 'ApiController@getitemstock', 'as' => 'getstock']);
-    Route::get('customers/autofetch', ['uses' => 'ApiController@fetchCustomer', 'as' => 'cust.get']);
-    Route::get('batch/amount', ['uses' => 'ApiController@fetchBatchAmount', 'as' => 'batch.amount']);
-    Route::get('credit/rate', ['uses' => 'ApiController@creditSelling', 'as' => 'credit.rate']);
-    Route::get('approve/adjustment', ['uses' => 'ApiController@approveStockAdjustment', 'as' => 'adj_approve']);
-});
--- Http/routes.php
+++ Http/routes.php
@@ -1,79 +1,72 @@
 <?php
 
-$route_params = [
-    'middleware' => [ 'auth.admin', 'setup'],
-    'prefix' => 'inventory',
-    'as' => 'inventory.',
-    'namespace' => 'Ignite\Inventory\Http\Controllers'];
-Route::group($route_params, function() {
-    Route::get('/', ['uses' => 'InventoryController@index', 'as' => 'index']);
+$router->get('/', ['uses' => 'InventoryController@index', 'as' => 'index']);
 
-    //sales
-    Route::match(['post', 'get'], 'shopfront', ['uses' => 'SalesController@shopfront', 'as' => 'shopfront']);
-    Route::get('sales/receipt/{id}', ['uses' => 'SalesController@receipt', 'as' => 'receipt']);
-    Route::get('sales/receipts', ['uses' => 'SalesController@receipts', 'as' => 'sales.receipts']);
+//sales
+$router->match(['post', 'get'], 'shopfront', ['uses' => 'SalesController@shopfront', 'as' => 'shopfront']);
+$router->get('sales/receipt/{id}', ['uses' => 'SalesController@receipt', 'as' => 'receipt']);
+$router->get('sales/receipts', ['uses' => 'SalesController@receipts', 'as' => 'sales.receipts']);
 
-    Route::match(['post', 'get'], 'sales/return', ['uses' => 'SalesController@return_goods', 'as' => 'sales.return']);
-    Route::get('sales/return/fillform', ['uses' => 'ApiController@fill_return_form', 'as' => 'sales.fillreturnform']);
-    Route::get('sales/return/fetchqtysold', ['uses' => 'ApiController@qty_sold', 'as' => 'sales.qtysold']);
-    //products
-    Route::match(['post', 'get'], 'categories/products/{id?}', ['uses' => 'InventoryController@product_categories', 'as' => 'product_categories']);
-    Route::match(['post', 'get'], 'categories/tax/{id?}', ['uses' => 'InventoryController@tax_categories', 'as' => 'tax_categories']);
-    Route::match(['post', 'get'], 'categories/measurement/{id?}', ['uses' => 'InventoryController@units_of_measurement', 'as' => 'units_of_measurement']);
-    Route::get('products/{id?}', ['uses' => 'InventoryController@products', 'as' => 'products']);
-    Route::match(['post', 'get'], 'manage/products/{id?}', ['uses' => 'InventoryController@add_product', 'as' => 'add_product']);
-    Route::get('store/products/available', ['uses' => 'InventoryController@view_stock', 'as' => 'view_stock']);
-    //stock_adjustments
-    Route::match(['get', 'post'], 'store/products/adjust/{id}', ['uses' => 'InventoryController@adjust_stock', 'as' => 'adjust_stock']);
-    Route::match(['get', 'post'], 'store/products/stock_adjustments', ['uses' => 'InventoryController@stock_adjustments', 'as' => 'stock.adjustments']);
-    //Discount, Price and Category Price
-    Route::get('product/discount', ['uses' => 'InventoryController@setProductDiscount', 'as' => 'product.discount']);
-    Route::get('product/discount/{id}/delete', ['uses' => 'InventoryController@delProductDiscount', 'as' => 'product.discount.del']);
+$router->match(['post', 'get'], 'sales/return', ['uses' => 'SalesController@return_goods', 'as' => 'sales.return']);
+$router->get('sales/return/fillform', ['uses' => 'ApiController@fill_return_form', 'as' => 'sales.fillreturnform']);
+$router->get('sales/return/fetchqtysold', ['uses' => 'ApiController@qty_sold', 'as' => 'sales.qtysold']);
+//products
+$router->match(['post', 'get'], 'categories/products/{id?}', ['uses' => 'InventoryController@product_categories', 'as' => 'product_categories']);
+$router->match(['post', 'get'], 'categories/tax/{id?}', ['uses' => 'InventoryController@tax_categories', 'as' => 'tax_categories']);
+$router->match(['post', 'get'], 'categories/measurement/{id?}', ['uses' => 'InventoryController@units_of_measurement', 'as' => 'units_of_measurement']);
+$router->get('products/{id?}', ['uses' => 'InventoryController@products', 'as' => 'products']);
+$router->match(['post', 'get'], 'manage/products/{id?}', ['uses' => 'InventoryController@add_product', 'as' => 'add_product']);
+$router->get('store/products/available', ['uses' => 'InventoryController@view_stock', 'as' => 'view_stock']);
+//stock_adjustments
+$router->match(['get', 'post'], 'store/products/adjust/{id}', ['uses' => 'InventoryController@adjust_stock', 'as' => 'adjust_stock']);
+$router->match(['get', 'post'], 'store/products/stock_adjustments', ['uses' => 'InventoryController@stock_adjustments', 'as' => 'stock.adjustments']);
+//Discount, Price and Category Price
+$router->get('product/discount', ['uses' => 'InventoryController@setProductDiscount', 'as' => 'product.discount']);
+$router->get('product/discount/{id}/delete', ['uses' => 'InventoryController@delProductDiscount', 'as' => 'product.discount.del']);
 
-    Route::get('product/price', ['uses' => 'InventoryController@setProductPrice', 'as' => 'product.price']);
-    Route::get('product/price/{id}/delete', ['uses' => 'InventoryController@delProductPrice', 'as' => 'product.price.del']);
-    Route::post('product/price/edit', ['uses' => 'InventoryController@editProductPrice', 'as' => 'product.price.edit']);
+$router->get('product/price', ['uses' => 'InventoryController@setProductPrice', 'as' => 'product.price']);
+$router->get('product/price/{id}/delete', ['uses' => 'InventoryController@delProductPrice', 'as' => 'product.price.del']);
+$router->post('product/price/edit', ['uses' => 'InventoryController@editProductPrice', 'as' => 'product.price.edit']);
 
-    Route::get('category/price', ['uses' => 'InventoryController@setCategoryPrice', 'as' => 'category.price']);
-    Route::get('category/price/{id}/delete', ['uses' => 'InventoryController@delCategoryPrice', 'as' => 'category.price.del']);
+$router->get('category/price', ['uses' => 'InventoryController@setCategoryPrice', 'as' => 'category.price']);
+$router->get('category/price/{id}/delete', ['uses' => 'InventoryController@delCategoryPrice', 'as' => 'category.price.del']);
 ///
-    Route::match(['post', 'get'], 'item/price/save', ['uses' => 'InventoryController@saveItemPrices', 'as' => 'product.save_prod_price']);
-    Route::match(['post', 'get'], 'product/discount/save', ['uses' => 'InventoryController@saveProductDiscount', 'as' => 'product.save_discount']);
-    Route::match(['post', 'get'], 'category/price/save', ['uses' => 'InventoryController@saveCategoryPrice', 'as' => 'category.save_price']);
-    Route::match(['post', 'get'], 'product/markup', ['uses' => 'InventoryController@markup', 'as' => 'product.markup']);
-    Route::get('markup/{id}/delete', ['uses' => 'InventoryController@delMarkup', 'as' => 'product.markup.del']);
-    //purchases add_items_lpo
-    Route::get('purchase/order/details/{id}', ['uses' => 'InventoryController@order_details', 'as' => 'order_details']);
-    Route::match(['post', 'get'], 'purchase/paymnent/terms/{id?}', ['uses' => 'InventoryController@payment_terms', 'as' => 'payment_terms']);
-    Route::match(['post', 'get'], 'purchase/orders', ['uses' => 'InventoryController@purchase_orders', 'as' => 'purchase_orders']);
-    Route::match(['post', 'get'], 'internalorders', ['uses' => 'InventoryController@internal_orders', 'as' => 'orders.internal']);
-    Route::match(['post', 'get'], 'purchase/manager', ['uses' => 'InventoryController@add_lpo', 'as' => 'new_lpo']);
-    Route::match(['get', 'post'], 'purchase/receive/{ref}', ['uses' => 'InventoryController@receive_lpo', 'as' => 'receive_from_lpo']);
-    Route::match(['post', 'get'], 'purchase/direct/goods', ['uses' => 'InventoryController@receive_direct', 'as' => 'receive_direct']);
-    Route::get('goods/receive', ['uses' => 'InventoryController@receive_goods', 'as' => 'receive_goods']);
-    Route::get('goods/received/grns', ['uses' => 'InventoryController@grns', 'as' => 'grns']);
-    Route::get('batch/{id}/details', ['uses' => 'InventoryController@batch_details', 'as' => 'batch.details']);
-    Route::get('goods/delivered/{batch}', ['uses' => 'InventoryController@purchase_details', 'as' => 'purchase_details']);
+$router->match(['post', 'get'], 'item/price/save', ['uses' => 'InventoryController@saveItemPrices', 'as' => 'product.save_prod_price']);
+$router->match(['post', 'get'], 'product/discount/save', ['uses' => 'InventoryController@saveProductDiscount', 'as' => 'product.save_discount']);
+$router->match(['post', 'get'], 'category/price/save', ['uses' => 'InventoryController@saveCategoryPrice', 'as' => 'category.save_price']);
+$router->match(['post', 'get'], 'product/markup', ['uses' => 'InventoryController@markup', 'as' => 'product.markup']);
+$router->get('markup/{id}/delete', ['uses' => 'InventoryController@delMarkup', 'as' => 'product.markup.del']);
+//purchases add_items_lpo
+$router->get('purchase/order/details/{id}', ['uses' => 'InventoryController@order_details', 'as' => 'order_details']);
+$router->match(['post', 'get'], 'purchase/paymnent/terms/{id?}', ['uses' => 'InventoryController@payment_terms', 'as' => 'payment_terms']);
+$router->match(['post', 'get'], 'purchase/orders', ['uses' => 'InventoryController@purchase_orders', 'as' => 'purchase_orders']);
+$router->match(['post', 'get'], 'internalorders', ['uses' => 'InventoryController@internal_orders', 'as' => 'orders.internal']);
+$router->match(['post', 'get'], 'purchase/manager', ['uses' => 'InventoryController@add_lpo', 'as' => 'new_lpo']);
+$router->match(['get', 'post'], 'purchase/receive/{ref}', ['uses' => 'InventoryController@receive_lpo', 'as' => 'receive_from_lpo']);
+$router->match(['post', 'get'], 'purchase/direct/goods', ['uses' => 'InventoryController@receive_direct', 'as' => 'receive_direct']);
+$router->get('goods/receive', ['uses' => 'InventoryController@receive_goods', 'as' => 'receive_goods']);
+$router->get('goods/received/grns', ['uses' => 'InventoryController@grns', 'as' => 'grns']);
+$router->get('batch/{id}/details', ['uses' => 'InventoryController@batch_details', 'as' => 'batch.details']);
+$router->get('goods/delivered/{batch}', ['uses' => 'InventoryController@purchase_details', 'as' => 'purchase_details']);
 
-    //suppliers
-    Route::match(['post', 'get'], 'suppliers/manage/{id?}', ['uses' => 'InventoryController@add_edit_suppliers', 'as' => 'manage_suppliers']);
-    Route::get('suppliers/view/{id?}', ['uses' => 'InventoryController@suppliers', 'as' => 'suppliers']);
-    Route::match(['post', 'get'], 'suppliers/invoice', ['uses' => 'InventoryController@supplier_invoice', 'as' => 'suppliers.invoice']);
-    Route::match(['post', 'get'], 'supplier/invoice/view/{id}', ['uses' => 'InventoryController@supplier_invoice_details', 'as' => 'suppliers.invoice.details']);
+//suppliers
+$router->match(['post', 'get'], 'suppliers/manage/{id?}', ['uses' => 'InventoryController@add_edit_suppliers', 'as' => 'manage_suppliers']);
+$router->get('suppliers/view/{id?}', ['uses' => 'InventoryController@suppliers', 'as' => 'suppliers']);
+$router->match(['post', 'get'], 'suppliers/invoice', ['uses' => 'InventoryController@supplier_invoice', 'as' => 'suppliers.invoice']);
+$router->match(['post', 'get'], 'supplier/invoice/view/{id}', ['uses' => 'InventoryController@supplier_invoice_details', 'as' => 'suppliers.invoice.details']);
 
-    //prints
-    Route::get('print/lpo/{id}', ['uses' => 'ReportController@print_lpo', 'as' => 'print_lpo']);
-    Route::get('direct/dnote/{id}', ['uses' => 'ReportController@dnote', 'as' => 'dnote']);
-    Route::get('lpo/dnote/{id}', ['uses' => 'ReportController@dnote_lpo', 'as' => 'dnote_lpo']);
-    Route::get('sales/receipt/print/{id}', ['uses' => 'ReportController@receipt', 'as' => 'sale.receipt.print']);
-    Route::get('sales/receipt/invoice/print/{id}', ['uses' => 'ReportController@invoice', 'as' => 'sale.invoice.print']);
-    //Approve LPO
-    Route::get('lpo/approve/{id}', ['uses' => 'InventoryController@approveLPO', 'as' => 'approveLPO']);
+//prints
+$router->get('print/lpo/{id}', ['uses' => 'ReportController@print_lpo', 'as' => 'print_lpo']);
+$router->get('direct/dnote/{id}', ['uses' => 'ReportController@dnote', 'as' => 'dnote']);
+$router->get('lpo/dnote/{id}', ['uses' => 'ReportController@dnote_lpo', 'as' => 'dnote_lpo']);
+$router->get('sales/receipt/print/{id}', ['uses' => 'ReportController@receipt', 'as' => 'sale.receipt.print']);
+$router->get('sales/receipt/invoice/print/{id}', ['uses' => 'ReportController@invoice', 'as' => 'sale.invoice.print']);
+//Approve LPO
+$router->get('lpo/approve/{id}', ['uses' => 'InventoryController@approveLPO', 'as' => 'approveLPO']);
 
-    //reports
-    Route::match(['post', 'get'], 'reports/sales', ['uses' => 'ReportController@timePeriodSales', 'as' => 'reports.sales']);
-    Route::match(['post', 'get'], 'reports/stocks', ['uses' => 'ReportController@lowStocks', 'as' => 'reports.stocks']);
-    //Fetchers
+//reports
+$router->match(['post', 'get'], 'reports/sales', ['uses' => 'ReportController@timePeriodSales', 'as' => 'reports.sales']);
+$router->match(['post', 'get'], 'reports/stocks', ['uses' => 'ReportController@lowStocks', 'as' => 'reports.stocks']);
+//Fetchers
 
-    Route::get('suggest/items', ['uses' => 'ApiController@pullProductSuggestions', 'as' => 'prod.tulus']);
-});
+$router->get('suggest/items', ['uses' => 'ApiController@pullProductSuggestions', 'as' => 'prod.tulus']);
