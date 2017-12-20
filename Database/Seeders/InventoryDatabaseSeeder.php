affd828d54f041edc9dcd4091bbff6029b4ab930
Samuel Dervis <samueldervis@gmail.com>
2017-12-20 17:02:45 +0300

    Add a store seeder

--- Database/Seeders/InventoryDatabaseSeeder.php
+++ Database/Seeders/InventoryDatabaseSeeder.php
@@ -4,16 +4,19 @@ namespace Ignite\Inventory\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class InventoryDatabaseSeeder extends Seeder {
+class InventoryDatabaseSeeder extends Seeder
+{
 
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run() {
+    public function run()
+    {
         $this->call(ProductsTableSeeder::class);
         $this->call(SuppliersTableSeeder::class);
+        $this->call(StoreTableSeeder::class);
     }
 
 }
