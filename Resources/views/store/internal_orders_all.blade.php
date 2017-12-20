6e530f432bafe9f21f5325f5d7dd598afadc6c5c
Samuel Dervis <samueldervis@gmail.com>
2017-12-20 14:12:27 +0300

    Dispatched links

--- Resources/views/store/internal_orders_all.blade.php
+++ Resources/views/store/internal_orders_all.blade.php
@@ -1,13 +1,5 @@
 <?php
-/*
- * =============================================================================
- *
- * Collabmed Solutions Ltd
- * Project: iClinic
- * Author: Bravo Gidi (bkiptoo@collabmed.com)
- *
- * =============================================================================
- */
+extract($data);
 ?>
 
 @extends('layouts.app')
@@ -16,75 +8,79 @@
 @section('content_description','')
 
 @section('content')
-<div class="box box-info">
-    <div class="box-header with-border">
-        <h3 class="box-title"><a class="btn btn-primary btn-xs" href="{{route('inventory.store.new_order')}}">
-                <i class="fa fa-plus-square"></i> New Internal Order
-            </a>
-        </h3>
-    </div>
-    <div class="box-body">
-        <div class="row">
-            <div class="col-md-12">
-                @if(!$data['orders']->isEmpty())
-                <table class="table table-responsive table-striped">
-                    <tbody>
-                        @foreach($data['orders'] as $item)
-                        <tr id="supplier{{$item->id}}">
-                            <td>{{$loop->iteration}}</td>
-                            <td>{{$item->users->username}}</td>
-                            <td>{{$item->disp_store->name}}</td>
-                            <td>{{$item->rq_store->name}}</td>
-                            <td>
-                                @if($item->status ===0)
-                                <span  class="btn-info btn-xs">
+    <div class="box box-info">
+        <div class="box-header with-border">
+            <h3 class="box-title"><a class="btn btn-primary btn-xs" href="{{route('inventory.store.new_order')}}">
+                    <i class="fa fa-plus-square"></i> New Internal Order
+                </a>
+            </h3>
+        </div>
+        <div class="box-body">
+            <div class="row">
+                <div class="col-md-12">
+                    @if(!$orders->isEmpty())
+                        <table class="table table-responsive table-striped">
+                            <tbody>
+                            @foreach($orders as $item)
+                                <tr id="supplier{{$item->id}}">
+                                    <td>{{$loop->iteration}}</td>
+                                    <td>{{$item->users->profile->name}}</td>
+                                    <td>{{$item->disp_store->name}}</td>
+                                    <td>{{$item->rq_store->name}}</td>
+                                    <td>
+                                        @if($item->status ===0)
+                                            <span class="btn-info btn-xs">
                                     <small>
                                         <i class="fa fa-hourglass-start" aria-hidden="true"></i>
                                         pending
                                     </small>
                                 </span>
-                                @else
-                                <span  class="btn-success btn-xs">
+                                        @else
+                                            <span class="btn-success btn-xs">
                                     <small>
                                         <i class="fa fa-hourglass-start" aria-hidden="true"></i>
                                         closed
                                     </small>
                                 </span>
-                                @endif
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>User</th>
-                            <th>Dispatching Store</th>
-                            <th>Requesting Store</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                </table>
-                @else
-                <div class="alert alert-info">
-                    <p>No records to show</p>
+                                        @endif
+                                    </td>
+                                    <td><a href="{{route('inventory.store.view_orders',$item->id)}}">
+                                            <i class="fa fa-eye"></i> View
+                                        </a></td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>User</th>
+                                <th>Dispatching Store</th>
+                                <th>Requesting Store</th>
+                                <th>Status</th>
+                                <td>View</td>
+                            </tr>
+                            </thead>
+                        </table>
+                    @else
+                        <div class="alert alert-info">
+                            <p>No records to show</p>
+                        </div>
+                    @endif
                 </div>
-                @endif
             </div>
         </div>
     </div>
-</div>
-<script type="text/javascript">
-    $(document).ready(function () {
-        try {
-            $('table').DataTable({
-                dom: 'Bfrtip',
-                buttons: [
-                    'excel', 'pdf', 'print'
-                ]
-            });
-        } catch (e) {
-        }
-    });
-</script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            try {
+                $('table').DataTable({
+                    dom: 'Bfrtip',
+                    buttons: [
+                        'excel', 'pdf', 'print'
+                    ]
+                });
+            } catch (e) {
+            }
+        });
+    </script>
 @endsection
--- Resources/views/store/order_details.blade.php
+++ Resources/views/store/order_details.blade.php
@@ -49,25 +49,6 @@ extract($data);
                     </tfoot>
                 </table>
             </div>
-            <div class="box-footer">
-                <div class="btn-group ">
-                    @if($order->status != 0)
-                        <a href="{{route('inventory.print_lpo',$order->id)}}" target="_blank" class="btn btn-primary">
-                            <i class="fa fa-print"></i> Print</a>
-                    @endif
-                    @if($order->status == 0)
-                        <a href="{{route('inventory.approveLPO',$order->id)}}" class="btn btn-info">
-                            <i class="fa fa-check-circle-o"></i>Approve</a>
-                    @elseif($order->status == 1)
-                    <!--
-                <a href="{{route('inventory.to_collabmed',$order->id)}}" class="btn btn-warning">
-                    <i  class="fa fa-list"></i> Send to Collabmed</a>
-                -->
-                        <a href="{{route('inventory.receive_from_lpo',$order->id)}}" class="btn btn-success">
-                            <i class="fa fa-hand-lizard-o"></i> Receive Goods</a>
-                    @endif
-                </div>
-            </div>
         </div>
     </div>
 @endsection
