f89f732e4c6fbca67e245c9c34df18d8df3afd9f
kisiara <kisiarafrancis@gmail.com>
2018-01-29 11:20:19 +0300

    Added  the orders made tab

--- Resources/views/store/orders/includes/dashboard.blade.php
+++ Resources/views/store/orders/includes/dashboard.blade.php
@@ -21,8 +21,8 @@
 
     <div class="col-md-3">
         <div class="panel panel-info">
-            <div class="panel-heading">Orders Received
-                <a href="{{ route('inventory.store.orders-received', $store->id) }}" class="btn btn-xs btn-info pull-right">go</a>
+            <div class="panel-heading">Orders Made
+                <a href="{{ route('inventory.store.orders-made', $store->id) }}" class="btn btn-xs btn-info pull-right">go</a>
             </div>
             {{--<div class="panel-body">Panel Content</div>--}}
         </div>
